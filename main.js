3efb751f40b5e1c68dd52e3c41c8ab70aa90d508
tahmid-emam <0.1tahmidemam@gmail.com>
2023-11-24 10:40:12 +0600

    front end code added

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 const express = require('express')
 const app = express()
+const path=require('path')
+const cors=require('cors')
 
+// other packages are imported here
 const mongoose = require('mongoose')
 require('dotenv').config()
-
 const router = require('./src/routes/api')
 
 
 let DB_Port = process.env.DB_Port
-
 app.use(express.json())
+app.use(cors())
 
+
+// connected to the database
 mongoose.connect(DB_Port)
 
-
+// router is used here
 app.use('/api/v1', router)
 
 
-
+// wrong url goes here
 app.get('*', (req, res) => {
-    res.send('you entered in a wrong URL')
+    res.sendFile(path.resolve(__dirname,'client', 'my-app', 'index.html'))
 })
 
 module.exports = app
